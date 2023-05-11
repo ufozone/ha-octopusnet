301505d78b4bc30dbdc5e228efc902f37de76ddb
Markus <Markus@Markus-HP450G4>
2023-05-11 12:23:40 +0200

    add constants

---
+++ custom_components/octopusnet/const.py
@@ -0,0 +1,34 @@
+"""Constants for Digital Devices Octopus NET integration."""
+from logging import getLogger
+
+import voluptuous as vol
+from homeassistant.const import (
+    Platform,
+    CONF_DEVICE_ID,
+)
+from homeassistant.helpers import config_validation as cv
+
+LOGGER = getLogger(__package__)
+
+DOMAIN = "octopusnet"
+MANUFACTURER = "Digital Devices"
+
+PLATFORMS = [
+    #Platform.BINARY_SENSOR,
+    #Platform.SENSOR,
+]
+
+UPDATE_INTERVAL = 120
+
+SERVICE_REBOOT = "set_profile"
+SERVICE_REBOOT_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_DEVICE_ID): cv.entity_ids_or_uuids,
+    }
+)
+SERVICE_EPG_SCAN = "epg_scan"
+SERVICE_EPG_SCAN_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_DEVICE_ID): cv.entity_ids_or_uuids,
+    }
+)
