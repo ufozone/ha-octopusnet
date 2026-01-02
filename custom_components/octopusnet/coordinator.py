b2856516319b5fa2a10c4a950a288aebe387a47e
ufozone <dev@ufozone.de>
2026-01-02 12:03:59 +0100

    Remove unnecessary dependendy import

--- custom_components/octopusnet/coordinator.py
+++ custom_components/octopusnet/coordinator.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 import asyncio
 
 from datetime import timedelta
-from awesomeversion import AwesomeVersion
 
 from homeassistant.core import HomeAssistant
 from homeassistant.const import (
